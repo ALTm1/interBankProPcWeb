3653e521923df6ace89d3d622f3b1b4152c87a6e
xushuai <1720285317@qq.com>
2020-07-29 18:22:06 +0800

    2020/7/29-2s

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-23 08:49:06
+ * @LastEditTime: 2020-07-29 18:18:56
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \interBankProPcWeb\src\components\index.js
+ */
 import Vue from 'vue'
 
 import {
@@ -6,7 +14,12 @@ import {
   IconSvg
 } from '@csii/vx-web-ext'
 
+// 引入自定义HeaderItem
+import HeaderItem from '@/components/headeritem'
+
 // 注意 有些组件使用异步加载会有影响
 Vue.component('ui-container-full', ContainerFull)
 Vue.component('ui-icon-fa', IconFa)
 Vue.component('ui-icon-svg', IconSvg)
+
+Vue.component('HeaderItem', HeaderItem)
